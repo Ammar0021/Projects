18db9442162a10d5918ee79dc20207f0a2de7c7a
Ammar <19arahman1@keshacademy.com>
2024-11-03 22:11:19 +0000

    Add files via upload

---
+++ Connect-Four.py
@@ -0,0 +1,24 @@
+#Connect Four
+#Notes:
+#Define game board
+#2 Players or 1 Player (vs Computer)
+#Placing a piece
+#Check for win,draw
+#Game loop
+
+import colorama
+import os
+from random import randint
+from time import sleep
+from colorama import Fore, Style
+from sys import exit
+
+
+colorama.init()
+
+
+
+
+
+
+
