741ae3c0417833319322cac07658cc468133f799
Ammar <19arahman1@keshacademy.com>
2024-11-17 18:05:07 +0000

    Update Connect-Four.py
    Added winning conditions

--- Connect-Four.py
+++ Connect-Four.py
@@ -41,20 +41,20 @@ def game_mode():
             if choice == "1":
                 print("Starting Single Player Mode!")
                 sleep(0.7)
-                return "single"
+                return "single" # Stores "single"in "mode"
             
             elif choice == "2":
                 print("Starting Two Player Mode!") 
                 sleep(0.7)
-                return "multi"
+                return "multi" # Stores "multi"in "mode"
              
             else:
                 raise ValueError("Invalid choice! Please enter 1 or 2 only.") 
         except ValueError as error:
-            print (str(error))             
+            print (error)             
     
 def initialise_board():
-    return [[' ' for _ in range(7)] for _ in range(6)]
+    return [[' ' for i in range(7)] for i in range(6)]
 
 
 def display_board(board):
@@ -63,11 +63,47 @@ def display_board(board):
     for row in board:
         print(Fore.CYAN + "| " + " | ".join(row) + " |")
         print(Fore.GREEN + "------------------------------" + Fore.RESET)
-        
-welcome_screen()        
-board = initialise_board()
-display_board(board)
-mode = game_mode()
+
+def winning_conditions():
+    win_conditions[]
+    
+    # Horizonal Conditions
+    for row in range(6):
+        for col in range(4):
+            win_conditions.append([(row, col), (row, col +1), (row, col +2), (row, col +3)])
+    
+    # Vertical Conditions
+    for row in range(3):
+        for col in range(7):
+            win_conditions.append([(row, col), (row +1, col)(row +2, col)(row +3, col)])
+    
+    # Diagonal Conditions (Down Right)
+    for row in range(3):
+        for col in range(4):
+            win_conditions.append([(row, col), (row + 1, col +1), (row +2, col +2), (row +3, col +3)])
+    
+    # Diagonal Conditions (Down Left)
+    for row in range(3):
+        for col in range(3, 7):
+            win_conditions.append([(row, col), (row + 1, col -1,) (row +2, col -2,), (row + 3, col -3)])
+
+    return win_conditions        
+            
+def check_win():
+    for condition in win_conditions:
+        for row, col in condition:
+            if board[row][col]!= player:
+                return False
+        return True        
+    
+def main():        
+    welcome_screen()        
+    board = initialise_board()
+    display_board(board)
+    mode = game_mode()
+
+if __name__ == "__main__":
+    main()
 
 
 
