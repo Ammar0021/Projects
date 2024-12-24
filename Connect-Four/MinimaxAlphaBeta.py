f82357d49178b929896071ccb5f497f51ab496de
Ammar <19arahman1@keshacademy.com>
2024-12-24 03:12:38 +0000

    Updated MinimaxAlphaBeta.py

--- Connect-Four/MinimaxAlphaBeta.py
+++ Connect-Four/MinimaxAlphaBeta.py
@@ -1,14 +1,18 @@
 from main import *
 
+MAX_DEPTH = 7  # Avoids the risk of Stack Overflow, 7 balances performance and decisions (I think)
 
 def MinimaxAlphaBeta(board, depth, alpha, beta, MaximisingPlayer, player, computer):
-    win_conditions = winning_conditions
+    if depth <= 0:
+        return 0
+    
+    win_conditions = winning_conditions()
     
     if check_win(board, win_conditions, computer):
-        return 100 - depth # Makes the Algorithm find quicker wins
+        return 100 - depth # Encourages the Algorithm find quicker wins
     if check_win(board, win_conditions, player):
-        return 100 - depth # Makes algorithm avoid Quicker losses
-    if all(board[col][0] != ' ' for col in range(7)):
+        return 100 - depth # Encourages algorithm avoid moves leading to loss
+    if all(board[col][0] != ' ' for col in range(7) for row in range(6)):
         return 0 # Draw
     
     if MaximisingPlayer:
@@ -19,10 +23,38 @@ def MinimaxAlphaBeta(board, depth, alpha, beta, MaximisingPlayer, player, comput
                     if board[col][row] == ' ': # Finds empty cell
                         board[col][row] = computer # Places disc in empty cell
                         
-                        eval = MinimaxAlphaBeta(board, depth-1, alpha, beta, True, player, computer) # Recursive Call
-                        '''depth-1 makes algorithm reduce the depth each recursive call
-                        alpha is best value that maximiser (computer) can currently guarantee, (beta is opposite)
-                        True means move is being made by maximiser (computer)'''
+                        eval = MinimaxAlphaBeta(board, depth-1, alpha, beta, not MaximisingPlayer, player, computer) # Recursion
+                        '''
+                        - depth-1 makes algorithm reduce the depth each recursive call, allows to explore deeper
+                        - alpha is best value that maximiser (computer) can currently guarantee
+                        - beta is current best value that minimiser can guarantee
+                        - not MaximisingPlayer flips the current turn
+                        '''
                         
+                        MaxEval = max(MaxEval, eval)
+                        alpha = max(alpha, eval)
+                        
+                        if beta <= alpha: # Triggers pruning
+                            break
+                        board[col][row] = ' '
+        return MaxEval
+    
+    else:
+        MinEval = float('inf')
+        for col in range(7):
+            if board[col][0] == ' ':
+                for row in reversed(range(6)):
+                    if board[col][row] == ' ':
+                        board[col][row] = player
+                        
+                        eval = MinimaxAlphaBeta(board, depth-1, alpha, beta, not MaximisingPlayer, player, computer)
+                        MinEval = min(MinEval, eval)
+                        beta = min(beta, eval)
+                        
+                        if beta <= alpha:
+                            break
+                        board[col][row] = ' '
+        return MinEval
+    
                         
     
