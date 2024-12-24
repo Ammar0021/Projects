dcbbb19f5c6b0245f51ef0f40b13ed658c7da4af
Ammar <19arahman1@keshacademy.com>
2024-12-24 01:14:07 +0000

    Add files via upload

---
+++ Connect-Four/Misc.py
@@ -0,0 +1,29 @@
+
+'''Reminders:
+
+Define game board (6 Rows, 7 Columns)
+42 total discs (7x7)
+Add both singe and multiplayer mode, create new function for handling choice
+Logic for Placing a piece
+add function for winning conditions
+handle condition controlled Game loop
+add Computer Difficulty
+Update other function arguments
+Update main()
+Add score counter (learn lambda)
+Add only 1 "Draws" for multiplayer'''
+
+'''Issues:
+
+Losses Counter not updating
+Index out of range error (idk why ffs)'''
+
+'''Minimax:
+
+Scrap def computer_asian()
+revamp def computer hard and medium mode'''
+
+'''GUI (Pygame)
+
+lol'''
+
